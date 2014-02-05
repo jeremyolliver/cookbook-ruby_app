3fc72f5feec2dbaa42fa13315d35c0bf0b4fb9ea
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-02-05 15:30:46 +1300

    Fix metadata syntax

--- metadata.rb
+++ metadata.rb
@@ -20,16 +20,16 @@ end
 recipe "ruby_app::default",    "Installs ruby"
 recipe "ruby_app::extensions", "Installs development headers for common ruby libraries with c-extensions"
 
-attribute 'rubies'
-  :display_name => 'ruby versions'
-  :description => 'the ruby versions to install'
+attribute 'rubies',
+  :display_name => 'ruby versions',
+  :description => 'the ruby versions to install',
   :type => 'hash',
-  :recipes => ['ruby_app::default']
+  :recipes => ['ruby_app::default'],
   :default => { '2.1.0' => true }
 
-attribute 'ruby/bundler/version'
-  :display_name => 'bundler version'
-  :description => 'the version of bundler to install in each ruby'
+attribute 'ruby/bundler/version',
+  :display_name => 'bundler version',
+  :description => 'the version of bundler to install in each ruby',
   :type => 'string',
-  :recipes => ['ruby_app::default']
+  :recipes => ['ruby_app::default'],
   :default => '1.5.2'
