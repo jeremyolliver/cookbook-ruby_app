bcb41156da18767ed3137d4a8a70021822e475b8
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-02-10 11:20:07 +1300

    WIP - fuller integration tests, by installing gems to use said extension libs
    centos 5.9 version of imagemagick is too old for rmagick to install
    also runs slow, calling 'gem install' each kitchen verify run

--- test/integration/extensions/serverspec/extensions_spec.rb
+++ test/integration/extensions/serverspec/extensions_spec.rb
@@ -5,19 +5,30 @@ require_relative 'spec_helper'
 describe 'extensions' do
 
   describe 'xml-packages' do
-    # TODO: should we install the nokogiri gem as well?
+
     describe command('xml2-config --version') do
       it { should return_exit_status 0 }
       it { should return_stdout(/\d+\.\d+\.\d+/) }
     end
+
     describe command('xslt-config --version') do
       it { should return_exit_status 0 }
       it { should return_stdout(/\d+\.\d+\.\d+/) }
     end
+
+    describe 'nokogiri gem' do
+      let(:nokogiri_installation) { command('/opt/chef/embedded/bin/gem install nokogiri') }
+
+      it 'should install' do
+        expect(nokogiri_installation).to return_exit_status 0
+        expect(nokogiri_installation).to return_stdout(/Successfully installed nokogiri/)
+      end
+    end
+
   end
 
   describe 'image-magick' do
-    # TODO: should we install rmagick or mini_magick gem as well?
+
     describe command('iconv --version') do
       it { should return_exit_status 0 }
       it { should return_stdout(/\d+\.\d+/) }
@@ -28,10 +39,26 @@ describe 'extensions' do
       it { should return_stdout(/\d+\.\d+\.\d+/) }
     end
 
+    describe command('convert --version') do
+      it { should return_exit_status 0 }
+      it { should return_stdout(/\d+\.\d+\.\d+/) }
+    end
+
     describe file('/usr/bin/identify') do
       it { should be_file }
       it { should be_executable }
     end
+
+    describe 'rmagick gem' do
+      # TODO: fix installation on centos - probably needs path specified
+      let(:rmagick_install) { command('/opt/chef/embedded/bin/gem install rmagick') }
+
+      it 'should install' do
+        expect(rmagick_install).to return_exit_status 0
+        expect(rmagick_install).to return_stdout(/Successfully installed rmagick/)
+      end
+    end
+
   end
 
 end
