8954fb2a3f7ff3fbc45f37763d0b9f2ac0c6b5f1
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-02-11 15:34:21 +1300

    Adhere to ruby style guide

--- libraries/ruby_app_helpers.rb
+++ libraries/ruby_app_helpers.rb
@@ -1,17 +1,19 @@
-module RubyAppHelpers
+# Encoding: utf-8
 
+# Helper methods encapsulating logic that will be used in the recipe
+module RubyAppHelpers
   # This ensures that if the Chef::Config[:http_proxy] or Chef::Config[:https_proxy]
   # options are set, that they will be included in the environment_options returned
   # and set into the currently running chef ruby ENV variables
   def self.environment_options
     opts = {}
     [:http_proxy, :https_proxy].each do |config_opt|
-      if value = Chef::Config[config_opt]
-        ENV[config_opt.to_s.upcase] = value
-        opts[config_opt.to_s] = value
+      setting = Chef::Config[config_opt]
+      if setting
+        ENV[config_opt.to_s.upcase] = setting
+        opts[config_opt.to_s] = setting
       end
     end
     opts
   end
-
 end
