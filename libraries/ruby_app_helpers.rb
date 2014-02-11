f68fa7568dca8a4d9ed9d387af8f5bb52f23213e
Jeremy Olliver <jeremy.olliver@gmail.com>
2014-02-11 14:26:00 +1300

    WIP - support installing ruby through http(s) proxy

---
+++ libraries/ruby_app_helpers.rb
@@ -0,0 +1,17 @@
+module RubyAppHelpers
+
+  # This ensures that if the Chef::Config[:http_proxy] or Chef::Config[:https_proxy]
+  # options are set, that they will be included in the environment_options returned
+  # and set into the currently running chef ruby ENV variables
+  def self.environment_options
+    opts = {}
+    [:http_proxy, :https_proxy].each do |config_opt|
+      if value = Chef::Config[config_opt]
+        ENV[config_opt.to_s.upcase] = value
+        opts[config_opt] = value
+      end
+    end
+    opts
+  end
+
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -23,8 +23,10 @@ include_recipe 'ruby_build'
 
 node['rubies'].each do |ruby_version_string, is_enabled|
   if is_enabled
+    build_environment_options = RubyAppHelpers.environment_options
     # install and compile ruby
     ruby_build_ruby ruby_version_string do
+      environment(build_environment_options)
       prefix_path "/opt/rubies/#{ruby_version_string}"
     end
 
